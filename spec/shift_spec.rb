b1c3fce3af117bcc022c3bf9c5b3376c50d7c288
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-08-08 17:19:51 -0600

    Add KeyManager/OffsetManager objects to exists test

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -6,7 +6,15 @@ RSpec.describe Shift do
   it "exists" do
     # key is a Hash
     # offset is a Hash
-    shift_1 = Shift.new(keys_hash, offset_hash)
+
+    # Create an instance of KeyManager
+    # to get the keys to pass into Shift
+    key_manager = KeyManager.new
+    # Create an instance of OffsetManager
+    # to get the keys to pass into Shift
+    offsets_manager = OffsetManager.new
+
+    shift_1 = Shift.new(key_manager, offsets_manager)
 
     expect(shift_1).to be_a(Shift)
   end
