edb85c77543e2316b1105a3a4956b933fa3a0397
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-08-08 20:43:38 -0600

    Create exists, character, and encrypt tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -6,4 +6,25 @@ RSpec.describe Enigma do
 
     expect(enigma).to be_an(Enigma)
   end
+
+  it "can have a character set of defined characters" do
+    enigma = Enigma.new
+
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    expect(enigma.character_set).to eq(expected)
+  end
+
+  xit "can encrypt a message with a key and date" do
+    enigma = Enigma.new
+
+    result =
+            {
+               encryption: "keder ohulw",
+               key: "02715",
+               date: "040895"
+             }
+
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq(result)
+  end
 end
