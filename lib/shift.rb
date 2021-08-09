ecf7b9688e027cd846ea8c06470901ae0e93ce52
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-08-08 18:24:35 -0600

    Add shift method

--- lib/shift.rb
+++ lib/shift.rb
@@ -10,4 +10,20 @@ class Shift
     @keys = key_manager.keys
     @offsets = offsets_manager.offsets
   end
+
+  def shift
+    # shift_hash = {
+    #                 a_shift: @keys[:a_key].to_i + @offsets[:a_offset].to_i,
+    #                 b_shift: @keys[:b_key].to_i + @offsets[:b_offset].to_i,
+    #                 c_shift: @keys[:c_key].to_i + @offsets[:c_offset].to_i,
+    #                 d_shift: @keys[:c_key].to_i + @offsets[:c_offset].to_i
+    #               }
+
+    a_shift = @keys[:a_key].to_i + @offsets[:a_offset].to_i
+    b_shift = @keys[:b_key].to_i + @offsets[:b_offset].to_i
+    c_shift = @keys[:c_key].to_i + @offsets[:c_offset].to_i
+    d_shift = @keys[:c_key].to_i + @offsets[:c_offset].to_i
+
+    shifts = [a_shift, b_shift, c_shift, d_shift]
+  end
 end
