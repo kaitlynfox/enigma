6229f61a7af83037534f940bc3e883781c994d79
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-08-09 16:47:05 -0600

    Add encrypt and decrypt testss along with their helper methods tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Enigma do
     expect(enigma.character_set).to eq(expected)
   end
 
-  xit "can encrypt a message with a key and date" do
+  it "can encrypt a message with a provided key and date" do
     enigma = Enigma.new
 
     result =
@@ -25,6 +25,75 @@ RSpec.describe Enigma do
                date: "040895"
              }
 
+    message = "hello world"
+
+    expect(enigma.downcase_message(message)).to eq("hello world")
     expect(enigma.encrypt("hello world", "02715", "040895")).to eq(result)
+    expect(enigma.encrypt("hello world", "02715", "040895")).to be_a(Hash)
+  end
+
+  it "can encrypt a message with a provided key" do
+    enigma = Enigma.new
+
+    result =
+            {
+               encryption: "nefau qdxly",
+               key: "02715",
+               date: "090821"
+             }
+
+    message = "hello world"
+
+    expect(enigma.downcase_message(message)).to eq("hello world")
+    expect(enigma.encrypt("hello world", "02715")).to eq(result)
+  end
+
+  it "can create managers needed for encryption" do
+    enigma = Enigma.new
+
+    key = "02715"
+    date = "090821"
+
+    expect(enigma.create_managers(key, date)).to be_a(Shift)
+  end
+
+  it "can downcase and split a message" do
+    enigma = Enigma.new
+
+    message = "HELLO WORLD"
+    expected = ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
+
+    expect(enigma.downcase_message_and_split(message)).to eq(expected)
+  end
+
+  it "can set shifts to negative values" do
+    enigma = Enigma.new
+
+    array = [2, 13, 9, 73]
+    expected = [-2, -13, -9, -73]
+
+    expect(enigma.set_array_shift_values_negative(array)).to eq(expected)
+  end
+
+  it "can decrypt a message" do
+    enigma = Enigma.new
+
+    result = {
+                decryption: "hello world",
+                key: "02715",
+                date: "040895"
+              }
+
+    # Takes in message, key, and date
+    expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq(result)
+
+    expected = {
+                  encryption: "keder ohulw",
+                  key: "02715",
+                  date: "040895"
+              }
+
+    # Takes in a message and key (use's today's date)
+    expect(enigma.decrypt(expected[:encryption], "02715")).to eq(result)
   end
 end
