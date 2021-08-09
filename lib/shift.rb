f4d63b43e495c6e1f4a47ff278fbaeb3d62e01e5
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-08-08 22:41:57 -0600

    Add shift_character, adjust_shift_value, and check_new_index_location methods

--- lib/shift.rb
+++ lib/shift.rb
@@ -12,18 +12,40 @@ class Shift
   end
 
   def shift
-    # shift_hash = {
-    #                 a_shift: @keys[:a_key].to_i + @offsets[:a_offset].to_i,
-    #                 b_shift: @keys[:b_key].to_i + @offsets[:b_offset].to_i,
-    #                 c_shift: @keys[:c_key].to_i + @offsets[:c_offset].to_i,
-    #                 d_shift: @keys[:c_key].to_i + @offsets[:c_offset].to_i
-    #               }
-
     a_shift = @keys[:a_key].to_i + @offsets[:a_offset].to_i
     b_shift = @keys[:b_key].to_i + @offsets[:b_offset].to_i
     c_shift = @keys[:c_key].to_i + @offsets[:c_offset].to_i
     d_shift = @keys[:c_key].to_i + @offsets[:c_offset].to_i
 
     shifts = [a_shift, b_shift, c_shift, d_shift]
+  end
+
+# Shift a character by however much the shift is to the right
+# Will return the new character
+  def shift_character(character, shift, character_set)
+    starting_point = character_set.index(character)
+
+    if starting_point.nil?
+      return character
+    end
+
+    adjusted_shift_value = adjust_shift_value(shift)
+    new_index_location = starting_point + adjusted_shift_value
+    new_index_location = check_new_index_location(new_index_location)
+    character_set[new_index_location]
+  end
+
+  def adjust_shift_value(shift)
+    if shift > 27
+      return (shift % 27)
+    end
+    shift
+  end
+
+  def check_new_index_location(new_index_location)
+    if new_index_location > 27
+      new_index_location = new_index_location - 27
+    end
+    new_index_location
   end
 end
