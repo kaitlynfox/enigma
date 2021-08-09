51503880d376ea1a13728c987c1e123b91175462
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-08-08 22:41:23 -0600

    Add shift_character test

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -1,6 +1,7 @@
 require './lib/shift'
 require './lib/key_manager'
 require './lib/offset_manager'
+require './lib/enigma'
 
 RSpec.describe Shift do
   it "exists" do
@@ -51,4 +52,39 @@ RSpec.describe Shift do
       expect(shift).to be_an(Integer)
     end
   end
+
+    it "can shift a character" do
+      # Create an instance of KeyManager to get the keys to pass into Shift
+      # Create an instance of OffsetManager to get the keys to pass into Shift
+      key_manager = KeyManager.new
+      offsets_manager = OffsetManager.new
+
+      # Create a shift that takes in keys and offsets
+      shift_1 = Shift.new(key_manager, offsets_manager)
+
+      # Create an enigma to access character_set
+      enigma = Enigma.new
+      # Access the character_set
+      character_set = enigma.character_set
+
+      character = "x"
+      shift = 100
+      expected = "p"
+
+      expect(shift_1.shift_character(character, shift, character_set)).to be_an(String)
+      expect(shift_1.shift_character(character, shift, character_set)).to eq(expected)
+      expect(shift_1.adjust_shift_value(shift)).to eq(19)
+
+      character = "x"
+      shift = 8
+      expected = "e"
+
+      expect(shift_1.adjust_shift_value(shift)).to eq(8)
+
+      character = "%"
+      shift = 8
+      expected = "e"
+
+      expect(shift_1.shift_character(character, shift, character_set)).to eq("%")
+    end
 end
