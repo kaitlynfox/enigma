779f1a9999eceac00a38ef52c8bd9ec4da738bcb
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-08-09 16:42:37 -0600

    Correct d_shift key and offset key names to [:d_key]/[:d_offset]

--- lib/shift.rb
+++ lib/shift.rb
@@ -6,22 +6,20 @@ class Shift
   attr_reader :keys,
               :offsets
 
-  def initialize(key_manager, offsets_manager)
+  def initialize(key_manager, offset_manager)
     @keys = key_manager.keys
-    @offsets = offsets_manager.offsets
+    @offsets = offset_manager.offsets
   end
 
   def shift
     a_shift = @keys[:a_key].to_i + @offsets[:a_offset].to_i
     b_shift = @keys[:b_key].to_i + @offsets[:b_offset].to_i
     c_shift = @keys[:c_key].to_i + @offsets[:c_offset].to_i
-    d_shift = @keys[:c_key].to_i + @offsets[:c_offset].to_i
+    d_shift = @keys[:d_key].to_i + @offsets[:d_offset].to_i
 
     shifts = [a_shift, b_shift, c_shift, d_shift]
   end
 
-# Shift a character by however much the shift is to the right
-# Will return the new character
   def shift_character(character, shift, character_set)
     starting_point = character_set.index(character)
 
@@ -43,7 +41,7 @@ class Shift
   end
 
   def check_new_index_location(new_index_location)
-    if new_index_location > 27
+    if new_index_location >= 27
       new_index_location = new_index_location - 27
     end
     new_index_location
