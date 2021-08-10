e518d3cf26562e3931f82b924327e57d10a1fc65
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-08-10 01:46:22 -0600

    Try and add tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe Enigma do
     expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
 
     expect(enigma.character_set).to eq(expected)
+    expect(enigma.character_set).to be_an(Array)
   end
 
   it "can encrypt a message with a provided key and date" do
@@ -39,7 +40,7 @@ RSpec.describe Enigma do
             {
                encryption: "nefau qdxly",
                key: "02715",
-               date: "090821"
+               date: "100821"
              }
 
     message = "hello world"
@@ -64,6 +65,7 @@ RSpec.describe Enigma do
     expected = ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
 
     expect(enigma.downcase_message_and_split(message)).to eq(expected)
+    expect(enigma.downcase_message_and_split(message)).to be_an(Array)
   end
 
   it "can set shifts to negative values" do
@@ -73,10 +75,37 @@ RSpec.describe Enigma do
     expected = [-2, -13, -9, -73]
 
     expect(enigma.set_array_shift_values_negative(array)).to eq(expected)
+    expect(enigma.set_array_shift_values_negative(array)).to be_an(Array)
   end
 
   it "can decrypt a message" do
     enigma = Enigma.new
+    key = "02715"
+    date = "040895"
+    ciphertext = "keder ohulw"
+    characters = ciphertext.downcase.split("")
+    key_manager = KeyManager.new(key)
+    offset_manager = OffsetManager.new(date)
+    shift_manager = Shift.new(key_manager, offset_manager)
+    array_of_shifts = shift_manager.shift
+
+    expect(array_of_shifts).to be_an(Array)
+
+    array_of_negative_shifts = array_of_shifts.map do |shift|
+      -(shift)
+    end
+
+    array_of_shifts_index_tracker = 0
+    encrypted_message = characters.map do |character|
+      if array_of_shifts_index_tracker > 3
+        array_of_shifts_index_tracker = 0
+      end
+    new_character = shift_manager.shift_character(character, array_of_negative_shifts[array_of_shifts_index_tracker], enigma.character_set)
+    array_of_shifts_index_tracker += 1
+    new_character
+
+    expect(new_character).to be_a(String)
+    end
 
     result = {
                 decryption: "hello world",
@@ -86,5 +115,7 @@ RSpec.describe Enigma do
 
     # Takes in message, key, and date
     expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq(result)
+    expect(enigma.decrypt("keder ohulw", "02715", "040895")).to be_a(Hash)
+    expect(enigma.create_managers("02715", "040895")).to be_a(Shift)
   end
 end
