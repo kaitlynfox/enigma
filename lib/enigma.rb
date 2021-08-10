0bb70e829c5dcf62f459a3c761dfeb137192d2c8
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-08-09 23:01:39 -0600

    Correct decrypt method return hash value with appropriate decrption value

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -13,27 +13,19 @@ class Enigma
     @offset_manager = OffsetManager.new
   end
 
-  # Create a Shift since there could be either a passed in key/date
-  # or there could be random key/date
-  # Set the shift with the key/date
   def encrypt(message, key = @key_manager.random_number, date = @offset_manager.generate_todays_date)
-    # key_manager = KeyManager.new(key)
-    # offset_manager = OffsetManager.new(date)
-    # shift_manager = Shift.new(key_manager, offset_manager)
     shift_manager = create_managers(key, date)
-    # downcased_message = downcase_message(message)
-    # characters = downcased_message.split("")
     characters = downcase_message_and_split(message)
     array_of_shifts = shift_manager.shift
 
     array_of_shifts_index_tracker = 0
     encrypted_message = characters.map do |character|
-        if array_of_shifts_index_tracker > 3
-          array_of_shifts_index_tracker = 0
-        end
-      new_character = shift_manager.shift_character(character, array_of_shifts[array_of_shifts_index_tracker], @character_set)
-      array_of_shifts_index_tracker += 1
-      new_character
+      if array_of_shifts_index_tracker > 3
+        array_of_shifts_index_tracker = 0
+      end
+    new_character = shift_manager.shift_character(character, array_of_shifts[array_of_shifts_index_tracker], @character_set)
+    array_of_shifts_index_tracker += 1
+    new_character
     end
 
     hash = {
@@ -65,20 +57,19 @@ class Enigma
 
     array_of_shifts_index_tracker = 0
     encrypted_message = characters.map do |character|
-        if array_of_shifts_index_tracker > 3
-          array_of_shifts_index_tracker = 0
-        end
-      new_character = shift_manager.shift_character(character, array_of_negative_shifts[array_of_shifts_index_tracker], @character_set)
-      array_of_shifts_index_tracker += 1
-      new_character
+      if array_of_shifts_index_tracker > 3
+        array_of_shifts_index_tracker = 0
+      end
+    new_character = shift_manager.shift_character(character, array_of_negative_shifts[array_of_shifts_index_tracker], @character_set)
+    array_of_shifts_index_tracker += 1
+    new_character
     end
 
     hash = {
-            decryption: "hello world",
-            key: "02715",
-            date: "040895"
+            decryption: encrypted_message.join,
+            key: key,
+            date: date
           }
-
   end
 
   def set_array_shift_values_negative(array)
@@ -87,5 +78,3 @@ class Enigma
     end
   end
 end
-
-#     # inherit from fileworker class (reading/writing from class)?
